3a1000010f2a5b508a0dc9cfb1f15f9929101470
Michael Mwai <michaeldmwai@gmail.com>
2024-10-25 11:21:34 +0300

    Confirned JS connected.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import "./style.css";
 
+const content = document.querySelector("#content");
+
+content.innerHTML = ` 
+<section id="main"></section>
+      <h2>Main Dishes</h2>
+      <ul>
+        <li>Salmon Pasta</li>
+        <li>Grilled Chicken Sandwich</li>
+        <li>Vegetable Stir-Fry</li>
+      </ul>
+    </section>
+    <section id="desserts">
+      <h2>Desserts</h2>
+      <ul>
+        <li>Chocolate Cake</li>
+        <li>Red Velvet Cake</li>
+        <li>Tiramisu</li>
+      </ul>
+    </section>
+    <section id="drinks">
+      <h2>Drinks</h2>
+      <ul>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>Water</li>
+      </ul>
+    </section>
+`;
+
 console.log("Connected");
